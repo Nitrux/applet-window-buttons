ad7b3d6266525eb7c11eeb6538a546e001e64fa2
Uri Herrera <hello@uriherrera.com>
2021-01-18 23:21:33 -0600

    ...

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 	cd files
 
 	wget -q \
-		"https://launchpad.net/~krisives/+archive/ubuntu/applet-window-buttons/+files/applet-window-buttons_2.0.8.0-0~202012310003~rev156~pkg3~ubuntu20.04.1_amd64.deb"
+		"https://launchpadlibrarian.net/514472961/applet-window-buttons_2.0.8.0-0~202012310003~rev156~pkg3~ubuntu20.04.1_amd64.deb"
 )
 
 
