59e75ffb8d4744b67bc5257a53a98860952154f6
Uri Herrera <hello@uriherrera.com>
2021-06-07 21:51:14 -0500

    update pkg

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 	cd files
 
 	wget -q \
-		"https://launchpadlibrarian.net/540974371/applet-window-buttons_2.0.9.0-0~202105290446~rev165~pkg3~ubuntu20.04.1_amd64.deb"
+		"https://launchpadlibrarian.net/540974374/applet-window-buttons_2.0.9.0-0~202105290446~rev165~pkg3~ubuntu21.04.1_amd64.deb"
 )
 
 
