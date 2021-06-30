c04f178295e0348092ef2403e7ec29400a0ddc6f
Uri Herrera <hello@uriherrera.com>
2021-06-30 03:58:08 -0500

    [ci skip]

--- travis-build.sh
+++ travis-build.sh
@@ -9,6 +9,3 @@
 		"https://launchpadlibrarian.net/540974374/applet-window-buttons_2.0.9.0-0~202105290446~rev165~pkg3~ubuntu21.04.1_amd64.deb"
 )
 
-
-wget -qO nru https://raw.githubusercontent.com/Nitrux/nitrux-repository-util/master/nitrux-repository-util.sh
-bash nru files/*
